80b7db2304498998fd1b6ed7008c354fa1d0d77d
Keerthan-N <110042672+Keerthan-N@users.noreply.github.com>
2023-07-10 17:34:39 +0530

    Update Emiter.js

--- Emiter.js
+++ Emiter.js
@@ -1,1 +1,13 @@
+const EventEmitter = require('events');
+const celebrity = new EventEmitter();
 
+celebrity.on('Race',(result) => { 
+  result === 'win' ? console.log('Race Won') : console.log('Race lost');
+});
+
+process.on('exit',(code)=>{
+    console.log('Exiting the code ',code);
+});
+
+
+celebrity.emit('Race','win');
